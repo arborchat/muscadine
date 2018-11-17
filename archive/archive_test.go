3520d1add644388403f5fd41083c08a680625974
Chris Waldon <christopher.waldon.dev@gmail.com>
2018-11-17 15:01:48 -0500

    Add tests for Last()

--- archive/archive_test.go
+++ archive/archive_test.go
@@ -23,6 +23,12 @@ func newOrSkip(t *testing.T) *archive.Archive {
 	return a
 }
 
+func addOrSkip(t *testing.T, a *archive.Archive, message *arbor.ChatMessage) {
+	if err := a.Add(message); err != nil {
+		t.Skip("Failed adding message to archive:", err)
+	}
+}
+
 // TestAddHasGet ensures that messages can be added to the Archive, then are
 // available for retrieval.
 func TestAddHasGet(t *testing.T) {
@@ -53,5 +59,60 @@ func TestAddHasGet(t *testing.T) {
 		t.Error("After adding message, Get() returned nil for that message")
 	} else if m.UUID != message.UUID || m.Content != message.Content || m.Parent != message.Parent || m.Username != message.Username || m.Timestamp != message.Timestamp {
 		t.Errorf("Added %v, but got non-equal %v from Get()", message, m)
+	}
+}
+
+// TestLast checks that the Last() method returns a sorted slice with a length less
+// than the requested length.
+func TestLast(t *testing.T) {
+	a := newOrSkip(t)
+	message := arbor.ChatMessage{
+		UUID:      "whatever",
+		Parent:    "something",
+		Content:   "a lame test",
+		Timestamp: 500000,
+		Username:  "Socrates",
+	}
+	message2 := message
+	message2.UUID += "2"
+	message2.Timestamp += 6
+	message3 := message
+	message3.UUID += "3"
+	message3.Timestamp -= 30
+	const (
+		histLen      = 5
+		shortLen     = 2
+		maliciousLen = -1
+	)
+	if last := a.Last(histLen); len(last) != 0 {
+		t.Errorf("Empty archive returned non-empty slice when length was %d", histLen)
+	}
+	if last := a.Last(maliciousLen); len(last) != 0 {
+		t.Errorf("Empty archive returned non-empty slice when length was %d", maliciousLen)
+	}
+	for index, m := range []arbor.ChatMessage{message, message2, message3} {
+		addOrSkip(t, a, &m)
+		if last := a.Last(histLen); len(last) != index+1 {
+			t.Errorf("archive with %d element(s) returned slice with length %d", index+1, len(last))
+		} else {
+			ordered := true
+			for i, k := range last {
+				if i == 0 {
+					continue
+				}
+				if k.Timestamp < last[i-1].Timestamp {
+					ordered = false
+				}
+			}
+			if !ordered {
+				t.Error("Slice from Last() not sorted", last)
+			}
+		}
+	}
+	if last := a.Last(shortLen); len(last) != shortLen {
+		t.Errorf("Requested Last of len %d, got %d", shortLen, len(last))
+	}
+	if last := a.Last(maliciousLen); len(last) != 0 {
+		t.Errorf("non-Empty archive returned non-empty slice when length was %d", maliciousLen)
 	}
 }
