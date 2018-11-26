fc406b8a2fd772108de556b84484e578a4ffea28
Government Experiment 6502-CATON <cameron.himes@hotmail.com>
2018-11-26 12:44:23 -0500

    finished notification engine
    Changes to be committed:
    	modified:   client.go

--- client.go
+++ client.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"time"
 
 	arbor "github.com/arborchat/arbor-go"
 	"github.com/arborchat/muscadine/archive"
@@ -72,8 +73,7 @@ func (c *Client) RecieveHandler(handler func(*arbor.ChatMessage)) {
 // This method makes notifications and handles all notification logic
 func notificationEngine(cli *Client, msg *arbor.ChatMessage) {
 	// is the message new?
-	// NOTE: this comparison is in the process of being added
-	if true {
+	if msg.Timestamp > (time.Now().Unix() - int64(1)) {
 		// do not reply to self
 		if cli.username != msg.Username {
 			toSend := msg.Username + ": " + msg.Content
