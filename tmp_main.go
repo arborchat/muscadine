24d5a93aa73673533fc02e3bc8ffa97ae5b4a1d5
Chris Waldon <christopher.waldon.dev@gmail.com>
2018-12-01 14:27:02 -0500

    Remove history helpers from main

--- tmp_main.go
+++ tmp_main.go
@@ -32,39 +32,6 @@ func notificationEngine(cli *Client, msg *arbor.ChatMessage) {
 	}
 }
 
-func loadHist(history tui.Archive, histfile string) {
-	if histfile != "" {
-		file, err := os.Open(histfile)
-		if err != nil {
-			log.Println("Error opening history", err)
-		} else {
-			defer file.Close()
-			if err = history.Load(file); err != nil {
-				log.Println("Error loading history", err)
-			} else {
-				log.Println("History loaded from", file.Name())
-			}
-		}
-	}
-}
-func saveHist(history tui.Archive, histfile string) {
-	if histfile != "" {
-		file, err := os.OpenFile(histfile, os.O_RDWR|os.O_CREATE, 0600)
-		if err != nil {
-			log.Println("Error opening history", err)
-		} else {
-			defer file.Close()
-			if err = file.Truncate(0); err != nil {
-				log.Println("Error truncating history", err)
-			}
-			if err = history.Persist(file); err != nil {
-				log.Println("Error saving history", err)
-			} else {
-				log.Println("History saved to", file.Name())
-			}
-		}
-	}
-}
 func main() {
 	var (
 		ui       UI
