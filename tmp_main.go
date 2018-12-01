d25926c6ef49a3782e168629138513e36123764e
Chris Waldon <christopher.waldon.dev@gmail.com>
2018-12-01 14:20:12 -0500

    Remove composer logic from main

--- tmp_main.go
+++ tmp_main.go
@@ -32,30 +32,6 @@ func notificationEngine(cli *Client, msg *arbor.ChatMessage) {
 	}
 }
 
-// Composer writes arbor protocol messages
-type Composer struct {
-	username string
-	sendChan chan<- *arbor.ProtocolMessage
-}
-
-// Reply sends a reply to `parent` with the given message content.
-func (c *Composer) Reply(parent, content string) error {
-	chat, err := arbor.NewChatMessage(content)
-	if err != nil {
-		return err
-	}
-	chat.Parent = parent
-	chat.Username = c.username
-	proto := &arbor.ProtocolMessage{ChatMessage: chat, Type: arbor.NewMessageType}
-	c.sendChan <- proto
-	return nil
-}
-
-// Query sends a query for the message with the given ID.
-func (c *Composer) Query(id string) {
-	c.sendChan <- &arbor.ProtocolMessage{Type: arbor.QueryType, ChatMessage: &arbor.ChatMessage{UUID: id}}
-}
-
 func loadHist(history tui.Archive, histfile string) {
 	if histfile != "" {
 		file, err := os.Open(histfile)
