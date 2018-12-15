f1a4a62869cd8bcb49a2c77ff7c999ae87d40324
Chris Waldon <christopher.waldon.dev@gmail.com>
2018-12-15 18:13:14 -0500

    Move most chat history logic into tui/history.go
    This is the first step in making the logic for controlling the chat
    history's view independent of the overall TUI logic. They've been
    comingled for a long time, and that has gradually made the TUI
    code harder to maintain. I'm hoping to cleanly separate them, but
    it will take more work to completely finish it.

---
+++ tui/history.go
@@ -0,0 +1,154 @@
+package tui
+
+import (
+	"time"
+
+	"github.com/jroimartin/gocui"
+)
+
+// History acts as a controller for a gocui.View that holds chat history.
+type History struct {
+	name string
+	// Each of these booleans represents whether or not a state change is requested next time
+	// the Layout method is invoked. This decouples the History type from the view that it manages
+	// except for Layout and the Action functions
+	focus, unfocus, render bool
+	*HistoryState
+}
+
+// NewHistory creates a new controller for an History view.
+func NewHistory(state *HistoryState) *History {
+	return &History{name: historyView, HistoryState: state}
+}
+
+// Focus lets the History perform any changes needed when it gains focus.
+func (h *History) Focus() error {
+	h.focus = true
+	return nil
+}
+
+// Unfocus lets the History perform any changes needed when it loses focus.
+func (h *History) Unfocus() error {
+	h.unfocus = true
+	return nil
+}
+
+// ActionCursorUp attempt to move the selected message upward through the message
+// history.
+func (h *History) ActionCursorUp(c *gocui.Gui, v *gocui.View) error {
+	h.CursorUp()
+	cursorStart, _ := h.CursorLines()
+	_, currentY := v.Origin()
+	for currentY > 0 && currentY+1 > cursorStart {
+		h.ActionScrollUp(c, v)
+		_, currentY = v.Origin()
+	}
+	h.reRender()
+	return nil
+}
+
+// ActionCursorDown attempt to move the selected message downward through the message
+// history.
+func (h *History) ActionCursorDown(c *gocui.Gui, v *gocui.View) error {
+	h.CursorDown()
+	_, cursorEnd := h.CursorLines()
+	_, currentY := v.Origin()
+	_, viewHeight := v.Size()
+	for currentY+viewHeight-1 <= cursorEnd {
+		h.ActionScrollDown(c, v)
+		_, currentY = v.Origin()
+	}
+	h.reRender()
+	return nil
+}
+
+// ActionScrollTop jumps to the top of the history.
+func (h *History) ActionScrollTop(c *gocui.Gui, v *gocui.View) error {
+	currentX, _ := v.Origin()
+	h.CursorBeginning()
+	h.reRender()
+	return v.SetOrigin(currentX, 0)
+}
+
+// ActionScrollBottom attempts to move the view to the end of the history.
+func (h *History) ActionScrollBottom(c *gocui.Gui, v *gocui.View) error {
+	currentX, currentY := v.Origin()
+	_, viewHeight := v.Size()
+	maxY := h.Height()
+	h.CursorEnd()
+	// ensure that we are both not already at the end *and* that the
+	// end is off-screen
+	if currentY < (maxY-1) && maxY > viewHeight {
+		return v.SetOrigin(currentX, maxY-viewHeight)
+	}
+	h.reRender()
+	return nil
+}
+
+// ActionScrollUp attempts to move the view upwards through the history.
+func (h *History) ActionScrollUp(c *gocui.Gui, v *gocui.View) error {
+	currentX, currentY := v.Origin()
+	if currentY > 0 {
+		return v.SetOrigin(currentX, currentY-1)
+	}
+	return nil
+}
+
+// ActionScrollDown attempts to move the view downwards through the history.
+func (h *History) ActionScrollDown(c *gocui.Gui, v *gocui.View) error {
+	currentX, currentY := v.Origin()
+	maxY := h.Height()
+	if currentY < (maxY - 1) {
+		return v.SetOrigin(currentX, currentY+1)
+	}
+	return nil
+}
+
+// reRender requests a redraw of the history at the next invocation of Layout()
+func (h *History) reRender() {
+	h.render = true
+}
+
+// renderView actually performs the work of redrawing the chat history into the given view
+func (h *History) renderView(v *gocui.View) error {
+	v.Clear()
+	if msg := h.Get(h.Current()); msg != nil {
+		timestamp := time.Unix(msg.Timestamp, 0).Local().Format(time.UnixDate)
+		v.Title = histViewTitlePrefix + " | Selected: " + timestamp
+	}
+	return h.Render(v)
+}
+
+// Layout is responsible for setting the desired view dimensions for the
+// History, but *not* for setting its position. That is handled by a higher-order
+// layout function.
+// Layout also executes any state changes requested by other methods, such as gaining focus
+// or clearing the History's contents.
+func (h *History) Layout(g *gocui.Gui) error {
+	// Set the view's dimensions
+	width, height := g.Size()
+	v, err := g.SetView(h.name, 0, 0, width-1, height-1)
+	if err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		// If we are creating the view for the first time, configure its settings
+		v.Frame = true
+		v.Title = "Chat History"
+
+	}
+	if h.focus {
+		g.SetCurrentView(h.name)
+		h.focus = false
+	} else if h.unfocus {
+		h.unfocus = false
+	}
+	if h.render {
+		h.render = false
+		err = h.renderView(v)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
--- tui/tui.go
+++ tui/tui.go
@@ -1,7 +1,6 @@
 package tui
 
 import (
-	"fmt"
 	"log"
 	"sync"
 	"time"
@@ -22,7 +21,7 @@ type TUI struct {
 	messages chan *arbor.ChatMessage
 	Composer
 	*Editor
-	histState      *HistoryState
+	*History
 	init           sync.Once
 	editMode       bool
 	connected      bool
@@ -43,11 +42,11 @@ func NewTUI(client Client) (*TUI, error) {
 	}
 
 	t := &TUI{
-		Gui:       gui,
-		messages:  make(chan *arbor.ChatMessage),
-		histState: hs,
-		Composer:  client,
-		Editor:    NewEditor(),
+		Gui:      gui,
+		messages: make(chan *arbor.ChatMessage),
+		History:  NewHistory(hs),
+		Composer: client,
+		Editor:   NewEditor(),
 	}
 	client.OnReceive(t.Display)
 	t.done = t.mainLoop()
@@ -74,7 +73,7 @@ func (t *TUI) manageConnection(c Connection) {
 			}
 			go func() {
 				for i := 0; i < 5; i++ {
-					if root, err := t.histState.Root(); err == nil {
+					if root, err := t.Root(); err == nil {
 						t.Composer.Reply(root, "[join]")
 						return
 					}
@@ -104,9 +103,13 @@ func (t *TUI) mainLoop() chan struct{} {
 		defer close(done)
 		defer t.Close()
 
-		makeHist := gocui.ManagerFunc(t.layout)
+		err := t.History.Focus()
+		if err != nil {
+			log.Println("Error focusing chat history", err)
+		}
+		t.History.reRender()
 		layout := gocui.ManagerFunc(BottomPrimaryLayout(historyView, editView))
-		t.SetManager(t.Editor, makeHist, layout)
+		t.SetManager(t.Editor, t.History, layout)
 
 		for _, binding := range t.Keybindings() {
 			if err := t.SetKeybinding(binding.View, binding.Key, binding.Modifier, binding.Handler); err != nil {
@@ -131,16 +134,16 @@ func (t *TUI) AwaitExit() {
 // update listens for new messages to display and redraws the screen.
 func (t *TUI) update() {
 	for message := range t.messages {
-		// can't do this inside the loop or it will bind the wrong value of
-		// `message` and will be prone to race conditions on whether the
-		// `New()` method is invoked before the value of `message` is
-		// reassigned.
-		err := t.histState.New(message)
+		err := t.New(message)
 		if err != nil {
 			log.Println(err)
 		}
-
 		t.reRender()
+		// trigger a layout update
+		t.Gui.Update(func(g *gocui.Gui) error {
+			return nil
+		})
+
 	}
 }
 
@@ -152,7 +155,7 @@ func (t *TUI) Display(message *arbor.ChatMessage) {
 // quit asks the TUI to stop running. Should only be called as
 // a keystroke or mouse input handler.
 func (t *TUI) quit(c *gocui.Gui, v *gocui.View) error {
-	if root, err := t.histState.Root(); err != nil {
+	if root, err := t.Root(); err != nil {
 		log.Println("Not notifying that we quit:", err)
 	} else {
 		t.Composer.Reply(root, "[quit]")
@@ -161,112 +164,13 @@ func (t *TUI) quit(c *gocui.Gui, v *gocui.View) error {
 	return gocui.ErrQuit
 }
 
-// cursorDown attempt to move the selected message downward through the message
-// history.
-func (t *TUI) cursorDown(c *gocui.Gui, v *gocui.View) error {
-	t.histState.CursorDown()
-	t.reRender()
-	_, cursorEnd := t.histState.CursorLines()
-	_, currentY := v.Origin()
-	_, viewHeight := v.Size()
-	for currentY+viewHeight-1 <= cursorEnd {
-		t.scrollDown(c, v)
-		_, currentY = v.Origin()
-	}
-	return nil
-}
-
-// cursorUp attempt to move the selected message upward through the message
-// history.
-func (t *TUI) cursorUp(c *gocui.Gui, v *gocui.View) error {
-	t.histState.CursorUp()
-	t.reRender()
-	cursorStart, _ := t.histState.CursorLines()
-	_, currentY := v.Origin()
-	for currentY > 0 && currentY+1 > cursorStart {
-		t.scrollUp(c, v)
-		_, currentY = v.Origin()
-	}
-	return nil
-}
-
-// scrollDown attempts to move the view downwards through the history.
-func (t *TUI) scrollDown(c *gocui.Gui, v *gocui.View) error {
-	currentX, currentY := v.Origin()
-	maxY := t.histState.Height()
-	if currentY < (maxY - 1) {
-		return v.SetOrigin(currentX, currentY+1)
-	}
-	return nil
-}
-
-// scrollBottom attempts to move the view to the end of the history.
-func (t *TUI) scrollBottom(c *gocui.Gui, v *gocui.View) error {
-	currentX, currentY := v.Origin()
-	_, viewHeight := v.Size()
-	maxY := t.histState.Height()
-	t.histState.CursorEnd()
-	t.reRender()
-	// ensure that we are both not already at the end *and* that the
-	// end is off-screen
-	if currentY < (maxY-1) && maxY > viewHeight {
-		return v.SetOrigin(currentX, maxY-viewHeight)
-	}
-	return nil
-}
-
-// scrollUp attempts to move the view upwards through the history.
-func (t *TUI) scrollUp(c *gocui.Gui, v *gocui.View) error {
-	currentX, currentY := v.Origin()
-	if currentY > 0 {
-		return v.SetOrigin(currentX, currentY-1)
-	}
-	return nil
-}
-
-// scrollTop jumps to the top of the history.
-func (t *TUI) scrollTop(c *gocui.Gui, v *gocui.View) error {
-	currentX, _ := v.Origin()
-	t.histState.CursorBeginning()
-	t.reRender()
-	return v.SetOrigin(currentX, 0)
-}
-
 // queryNeeded sends a batch of queries to the server to update the history.
 func (t *TUI) queryNeeded(c *gocui.Gui, v *gocui.View) error {
-	needed := t.histState.Needed(10)
+	needed := t.Needed(10)
 	for _, n := range needed {
 		t.Query(n)
 	}
 	return nil
-}
-
-// reRender forces a redraw of the historyView
-func (t *TUI) reRender() {
-	t.Update(func(g *gocui.Gui) error {
-		v, err := g.View(historyView)
-		if err != nil {
-			return err
-		}
-		v.Clear()
-		needed := t.histState.Needed(100)
-		var suffix string
-		if !t.connected {
-			suffix += "Connecting... "
-		} else {
-			suffix += "Connected, "
-		}
-		if len(needed) == 0 {
-			suffix += "all known threads complete"
-		} else {
-			suffix += fmt.Sprintf("%d+ broken threads, q to query", len(needed))
-		}
-		if msg := t.histState.Get(t.histState.Current()); msg != nil {
-			timestamp := time.Unix(msg.Timestamp, 0).Local().Format(time.UnixDate)
-			v.Title = histViewTitlePrefix + " | Selected: " + timestamp + " | " + suffix
-		}
-		return t.histState.Render(v)
-	})
 }
 
 // historyMode transitions the TUI to interactively scroll the history.
@@ -276,32 +180,32 @@ func (t *TUI) historyMode() error {
 	if err != nil {
 		return err
 	}
-	_, err = t.Gui.SetCurrentView(historyView)
-	if err != nil {
-		return err
-	}
-	return nil
+	return t.History.Focus()
 }
 
 // composeMode transitions the TUI to interactively editing messages.
 // All state change related to that transition should be defined here.
 func (t *TUI) composeMode(replyTo *arbor.ChatMessage) error {
+	err := t.History.Unfocus()
+	if err != nil {
+		return err
+	}
 	return t.Editor.Focus(replyTo)
 }
 
 // composeReply starts replying to the current message.
 func (t *TUI) composeReply(c *gocui.Gui, v *gocui.View) error {
-	msg := t.histState.Get(t.histState.Current())
+	msg := t.Get(t.Current())
 	return t.composeMode(msg)
 }
 
 // composeReplyToRoot starts replying to the earliest known message (root, unless something is very wrong).
 func (t *TUI) composeReplyToRoot(c *gocui.Gui, v *gocui.View) error {
-	root, err := t.histState.Root()
+	root, err := t.Root()
 	if err != nil {
 		return err
 	}
-	rootMsg := t.histState.Get(root)
+	rootMsg := t.Get(root)
 	return t.composeMode(rootMsg)
 }
 
@@ -322,32 +226,4 @@ func (t *TUI) sendReply(c *gocui.Gui, v *gocui.View) error {
 	v.SetOrigin(0, 0)
 	t.Composer.Reply(t.Editor.ReplyTo.UUID, content[:len(content)-1])
 	return t.historyMode()
-}
-
-// layout places views in the UI.
-func (t *TUI) layout(gui *gocui.Gui) error {
-	mX, mY := gui.Size()
-	histMaxX := mX - 1
-	histMaxY := mY - 1
-	histMaxY -= 3
-	t.histState.SetDimensions(histMaxY-1, histMaxX-1)
-	if t.lastKnownWidth != mX {
-		t.reRender()
-	}
-	t.lastKnownWidth = mX
-	// update view dimensions or create for the first time
-	histView, err := gui.SetView(historyView, 0, 0, histMaxX, histMaxY)
-	if err != nil {
-		if err != gocui.ErrUnknownView {
-			return err
-		}
-		// if you reach this point, you are initializing the view for the first time
-		histView.Title = "Chat History"
-
-		if _, err := t.SetCurrentView(historyView); err != nil {
-			log.Println("Failed to set historyView focus", err)
-		}
-	}
-
-	return nil
 }
