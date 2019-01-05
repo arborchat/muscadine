0dfa27e996d171838b26f76bb341bbc8501e1e21
Chris Waldon <christopher.waldon.dev@gmail.com>
2019-01-04 21:42:52 -0500

    Configure a log file by default

--- main.go
+++ main.go
@@ -2,27 +2,61 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
+	"os/user"
+	"path"
 
 	"github.com/arborchat/muscadine/archive"
 	"github.com/arborchat/muscadine/tui"
 	"github.com/arborchat/muscadine/types"
 )
 
+// getDefaultLogFile returns a path to the default muscadine log file location.
+// Right now, it makes naiive decisions that make the most sense on UNIX systems.
+// It could be improved significantly to incorporate the XDG Base Directory
+// Specification and a more typical path for Windows users.
+func getDefaultLogFile() string {
+	cwdFile := "muscadine.log"
+	u, err := user.Current()
+	if err != nil {
+		return cwdFile
+	}
+	return path.Join(u.HomeDir, ".local", "share", "muscadine", cwdFile)
+}
+
+// configureLogging attempts to set the global logger to use the named file, and logs
+// an error to stdout if it fails. It returns a teardown function that can be used to
+// clean up the logging and print a status message to the user.
+func configureLogging(logfile string) func() {
+	file, err := os.OpenFile(logfile, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		log.Printf("Unable to begin logging to %s, %v", logfile, err)
+		return func() {}
+	}
+	log.SetOutput(file)
+	return func() {
+		fmt.Fprintln(os.Stderr, "Logs written to", file.Name())
+		file.Close()
+	}
+}
+
 func main() {
 	var (
-		ui       types.UI
-		err      error
-		username string
-		histfile string
+		ui                types.UI
+		err               error
+		username          string
+		histfile, logfile string
 	)
 	flag.StringVar(&username, "username", "muscadine", "Set your username on the server")
 	flag.StringVar(&histfile, "histfile", "", "Load history from this file")
+	flag.StringVar(&logfile, "logfile", getDefaultLogFile(), "Write logs to this file")
 	flag.Parse()
 	if len(flag.Args()) < 1 {
 		log.Fatal("Usage: " + os.Args[0] + " <ip>:<port>")
 	}
+	defer configureLogging(logfile)() // defer the returned cleanup function
 	history, err := archive.NewManager(histfile)
 	if err != nil {
 		log.Fatalln("unable to construct archive", err)
