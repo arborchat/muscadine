308719330d86b8746dc11b8f486bcbd171b10c89
Chris Waldon <christopher.waldon.dev@gmail.com>
2019-01-15 19:01:32 -0500

    Suppress interactive editor

--- build-releases.sh
+++ build-releases.sh
@@ -2,22 +2,25 @@
 
 set -euo pipefail
 
+# only build on master
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] || [ "$TRAVIS_BRANCH" != "master" ]; then
     echo "Not building release, this is a pull request"
     exit 0
 fi
 
+# get hub if we don't have it
 if ! command -v hub 2>&1 > /dev/null ; then
     go get github.com/github/hub
 fi
 
-hub_flags=""
+release_flags=""
 head_commit="$(git rev-parse HEAD 2> /dev/null)"
 
+# build pre-release if not on a tag, otherwise build release
 if ! git describe --tags --exact-match HEAD > /dev/null 2>&1 ; then
     echo "Building pre-release, not tagged commit"
     readonly tag="release-$(echo ${head_commit} | head -c 7)"
-    hub_flags=" --prerelease "
+    release_flags="--prerelease"
 else 
     echo "Building release, tagged commit"
     readonly tag="$(git describe --tags --exact-match HEAD 2> /dev/null)"
@@ -25,8 +28,10 @@ fi
 
 readonly project="muscadine"
 readonly bin_name="$project"
+readonly message="Automatic build"
 
-hub release create $hub_flags  --message="Automatic build" --commitish="$head_commit" "$tag"
+# create the release and upload artifacts
+hub release create $release_flags --message="$message" --commitish="$head_commit" "$tag"
 for os in darwin linux windows openbsd; do
     archive_name="$project-$os.tar.gz"
     echo "Building $project for $os"
@@ -34,5 +39,5 @@ for os in darwin linux windows openbsd; do
     tar czf "$archive_name" "$bin_name" &&\
     rm "$bin_name"
     echo "Adding $archive_name to release $tag"
-    hub release edit  --attach="$archive_name#Muscadine for $os" "$tag"
+    hub release edit --message="$message" --attach="$archive_name#muscadine_${os}" "$tag"
 done
